e763e5b823787b6430b71ec272ac00e96fab442c
Kalle <kalle.kytola@aalto.fi>
2023-12-16 22:17:56 +0200

    Adding day 04 in the state when I didnt know how to use state monads.

---
+++ AoC2023Lean4/AoC2023d04p1.lean
@@ -0,0 +1,33 @@
+import AoC2023Lean4.AoC2023helpers
+/-
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+-/
+
+def readNumbers (data : String) : List Nat :=
+  listOpt $ String.toNat? <$> data.splitOn " "
+
+def splitWinningChosen (data : String) : List Nat × List Nat :=
+  let wc := (·.splitOn "|") <$> (data.splitOn ":")
+  (readNumbers $ optString (wc[1]? >>= (·[0]?)), readNumbers $ optString (wc[1]? >>= (·[1]?)))
+
+def hitCounter (targets data : List Nat) : Counter Nat := do
+  for t in targets do
+    if data.contains t then Counter.incr
+  pure (← get)
+
+def countHits (targets data : List Nat) : Nat := (hitCounter targets data).valFromInit
+
+def winOfHits (hits : Nat) : Nat :=
+  if hits == 0 then 0 else 2^(hits-1)
+
+def main : IO Unit := do
+  let probInput := (← IO.FS.readFile "./AoC2023Lean4/data-AoC2023d04p1.txt").trim.splitOn "\n"
+  let total := ((fun (w,c) => winOfHits $ countHits w c) <$> splitWinningChosen <$> probInput).sum
+  IO.println s!"Answer (points) : {total}"
+
+--#eval main
