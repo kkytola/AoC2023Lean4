1d5b3be6ef2520d1a7b69a50abd62f358bef838f
Kalle <kalle.kytola@aalto.fi>
2023-12-16 22:08:13 +0200

    Remove unnecessary import.

--- AoC2023Lean4/AoC2023d02p2.lean
+++ AoC2023Lean4/AoC2023d02p2.lean
@@ -1,5 +1,4 @@
 import AoC2023Lean4.AoC2023helpers
-import Mathlib.Tactic
 /-
 Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
